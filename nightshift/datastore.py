bebfad159093dbd1f3b7c3c06e48284aa6969799
klinga <klingaroo@gmail.com>
2020-10-05 15:33:32 -0400

    tables init

---
+++ nightshift/datastore.py
@@ -0,0 +1,146 @@
+"""
+Defines and initiates NightShift SQLite database
+"""
+
+from datetime import datetime
+
+from sqlalchemy import (
+    Boolean,
+    Date,
+    DateTime,
+    Column,
+    ForeignKey,
+    Integer,
+    PickleType,
+    String,
+    UniqueConstraint,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from schalchemy.inspection import inspect
+from sqlalchemy.orm import relationship, sessionmaker
+
+
+Base = declarative_base()
+
+
+class LibrarySystem(Base):
+    __tablename__ = "library_system"
+
+    lsid = Column(Integer, primary_key=True)
+    code = Column(String(3), nullable=False, unique=True)
+    name = Column(String(25))
+
+    def __repr__(self):
+        state = inspect(self)
+        attrs = ", ".join([f"{attr.key}={attr.loaded_value!r}" for attr in state.attrs])
+        return f"<LibrarySystem({attrs})>"
+
+
+class BibCategory(Base):
+    __tablename__ = "bib_category"
+
+    bcid = Column(Integer, primary_key=True)
+    code = Column(String(5), nullable=False, unique=True)
+    description = Column(String(50))
+
+    def __repr__(self):
+        state = inspect(self)
+        attrs = ", ".join([f"{attr.key}={attr.loaded_value!r}" for attr in state.attrs])
+        return f"<BibCategory({attrs})>"
+
+
+class ExportFile(Base):
+    __tablename__ = "export_file"
+
+    efid = Column(Integer, primary_key=True)
+    handle = Column(String(50), nullable=False)
+    dateCreated = Column(DateTime, nullable=False, default=datetime.now())
+
+    def __repr__(self):
+        state = inspect(self)
+        attrs = ", ".join([f"{attr.key}={attr.loaded_value!r}" for attr in state.attrs])
+        return f"<ExportFile({attrs})>"
+
+
+class OutputFile(Base):
+    __tablename__ = "output_file"
+
+    ofid = Column(Integer, primary_key=True)
+    handle = Column(String(50), nullable=False)
+    dateCreated = Column(DateTime, nullable=False, default=datetime.now())
+
+    def __repr__(self):
+        state = inspect(self)
+        attrs = ", ".join([f"{attr.key}={attr.loaded_value!r}" for attr in state.attrs])
+        return f"<OutputFile({attrs})>"
+
+
+class UpgradeSource(Base):
+    __tablename__ = "upgrade_source"
+
+    usid = Column(Integer, primary_key=True)
+    name = Column(String(8), nullable=False, unique=True)
+    description = Column(String(50))
+
+    def __repr__(self):
+        state = inspect(self)
+        attrs = ", ".join([f"{attr.key}={attr.loaded_value!r}" for attr in state.attrs])
+        return f"<UpgradeSource({attrs})>"
+
+
+class Resource(Base):
+    __tablename__ = "resource"
+    __table_args__ = UniqueConstraint("bid", "library_system_id", name="uix_resource")
+
+    bid = Column(Integer, primary_key=True, autoincrement=False)
+    library_system_id = Column(
+        Integer, ForeignKey("library_system.lsid"), nullable=False
+    )
+    bibCategoryId = Column(Integer, ForeignKey("bib_category.bcid"), nullable=False)
+    exportFileId = Column(Integer, ForeignKey("export_file.efid"), nullable=False)
+    outputFileId = Column(Integer, ForeignKey("output_file.ofid"))
+    sbn = Column(String(13))  # isbn
+    lcn = Column(String(15))  # lccn
+    did = Column(String(50))  # distributor number, ex. Overdrive reserve no.
+    sid = Column(String(15))  # other standard number, ex. UPC
+    wcn = Column(Integer)  # Worldcat OCLC number
+    bibDate = Column(Date, nullable=False)
+    title = Column(String(50), nullable=False)
+    author = Column(String(50))
+    pubDate = Column(Date)
+    upgradeStamp = Column(DateTime)
+    upgraded = Column(Boolean, default=False)
+    upgradeSourceId = Column(Integer, ForeignKey("upgrade_source.usid"))
+
+    def __repr__(self):
+        state = inspect(self)
+        attrs = ", ".join([f"{attr.key}={attr.loaded_value!r}" for attr in state.attrs])
+        return f"<Resource({attrs})>"
+
+
+class UrlField(Base):
+    __tablename__ = "url_field"
+
+    uid = Column(Integer, primary_key=True)
+    bibId = Column(Integer, ForeignKey("resource.bid"), nullable=False)
+    url = Column(PickleType, nullable=False)
+
+    def __repr__(self):
+        state = inspect(self)
+        attrs = ", ".join([f"{attr.key}={attr.loaded_value!r}" for attr in state.attrs])
+        return f"<UrlField({attrs})>"
+
+
+class WorldcatQuery(Base):
+    __tablename__ = "worldcat_query"
+
+    wqid = Column(Integer, primary_key=True)
+    bibId = Column(Integer, ForeignKey("resource.bid"), nullable=False)
+    queryStamp = Column(DateTime, nullable=False, default=datetime.now())
+    found = Column(Boolean, nullable=False, default=False)
+    wcResponse = Column(PickleType)
+
+    def __repr__(self):
+        state = inspect(self)
+        attrs = ", ".join([f"{attr.key}={attr.loaded_value!r}" for attr in state.attrs])
+        return f"<WorldcatQuery({attrs})>"
