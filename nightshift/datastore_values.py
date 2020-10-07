294ef7726d1a106ca15dee69dd6c95809f7f99bf
klinga <klingaroo@gmail.com>
2020-10-07 14:19:33 -0400

    bib categories added

--- nightshift/datastore_values.py
+++ nightshift/datastore_values.py
@@ -8,3 +8,8 @@ LIB_SYS = {
     "nyp": {"code": "nyp", "lsid": 1, "name": "New York Public Library"},
     "bpl": {"code": "bpl", "lsid": 2, "name": "Brooklyn Public Library"},
 }
+
+BIB_CAT = {
+    "ebk": {"code": "ebk", "bcid": 1, "description": "Overdrive e-resources"},
+    "bke": {"code": "pre", "bcid": 2, "description": "English print monograph"},
+}
