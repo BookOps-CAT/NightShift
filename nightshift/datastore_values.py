4fe39dbc03da5d97ae89cca44183bd1bd8c6b5a0
klinga <klingaroo@gmail.com>
2020-10-06 23:51:54 -0400

    LIB_SYS values

---
+++ nightshift/datastore_values.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Predefined datastore values
+"""
+
+LIB_SYS = {
+    "nyp": {"code": "nyp", "lsid": 1, "name": "New York Public Library"},
+    "bpl": {"code": "bpl", "lsid": 2, "name": "Brooklyn Public Library"},
+}
