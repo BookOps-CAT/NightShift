9dd85546c8819edaa3d3ac7ca4241cbd985955f2
klinga <klingaroo@gmail.com>
2020-10-28 22:31:44 -0400

    upgrade src added

--- nightshift/datastore_values.py
+++ nightshift/datastore_values.py
@@ -14,3 +14,16 @@ BIB_CAT = {
     "ebk": {"code": "ebk", "bcid": 1, "description": "Overdrive e-resources"},
     "pre": {"code": "pre", "bcid": 2, "description": "English print monograph"},
 }
+
+UPGRADE_SRC = {
+    "bot": {
+        "usid": 1,
+        "name": "bot",
+        "description": "full bib brought in by nightshift bot",
+    },
+    "staff": {
+        "usid": 2,
+        "name": "staff",
+        "description": "full bib brought in by staff",
+    },
+}
