54e9df69a8635f2fba8aa37d1bf54e8f51519c6c
klinga <klingaroo@gmail.com>
2020-10-30 02:30:00 -0400

    retrieve_bibnos_for_enhancement added

--- nightshift/workers.py
+++ nightshift/workers.py
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
+
 """
 This module includes individual bot's operations
 """
 import os
-from typing import Type
+from typing import List, Type
 
 import sqlalchemy
 
 from .export_file_parser import SierraExportReader
-from .datastore_transactions import insert_resource, insert_export_file
+from .datastore_transactions import insert_resource, insert_export_file, retrieve_bibnos
+from .datastore_values import LIB_SYS, BIB_CAT
 
 
 def record_export_file_data(fh: str, session: Type[sqlalchemy.orm.session.Session]):
@@ -15,7 +18,11 @@ def record_export_file_data(fh: str, session: Type[sqlalchemy.orm.session.Sessio
     Inserts export file record to ExportFile table
 
     Args:
-        fh:
+        fh:                     Sierra export file handle as path
+        session:                sqlalchemy session
+
+    Returns:
+        `nightshift.datastore.ExportFile` instance
     """
     handle = os.path.basename(fh)
     record = insert_export_file(session, handle=handle)
@@ -24,6 +31,13 @@ def record_export_file_data(fh: str, session: Type[sqlalchemy.orm.session.Sessio
 
 
 def import_sierra_data(fh: str, session: Type[sqlalchemy.orm.session.Session]):
+    """
+    Reads content of Sierra export and imports it to data.db
+
+    Args:
+        fh:                     Sierra export file handle as path
+        session:                sqlalchemy session
+    """
     # record and get table id of export file
     fh_rec = record_export_file_data(fh, session)
 
@@ -31,3 +45,27 @@ def import_sierra_data(fh: str, session: Type[sqlalchemy.orm.session.Session]):
     for res in data:
         insert_resource(session, **res._asdict(), exportFileId=fh_rec.efid)
     session.commit()
+
+
+def retrieve_bibnos_for_enhancement(
+    lib_sys: str, bib_cat: str, session: Type[sqlalchemy.orm.session.Session]
+) -> List[int]:
+    """
+    Queries datastore and returns Sierra bib numbers (sbid) of records
+    that are missing Overdrive resource id
+
+    Args:
+        lib_sys:                library system: 'nyp' or 'bpl'
+        bib_cat:                bib category: 'ere' (e-resouces) or
+                                'pre' (English print)
+        session:                sqlalchemy session
+
+    Returns:
+        list of bibNos
+    """
+    lsid = LIB_SYS[lib_sys]["lsid"]
+    bcid = BIB_CAT[bib_cat]["bcid"]
+
+    sierra_bibnos = retrieve_bibnos(session, lsid, bcid)
+
+    return sierra_bibnos
--- tests/test_workers.py
+++ tests/test_workers.py
@@ -1,18 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+worker module tests
+"""
+
+import pytest
+
 from nightshift.datastore import Resource, ExportFile
-from nightshift.workers import import_sierra_data, record_export_file_data
+from nightshift.workers import (
+    import_sierra_data,
+    record_export_file_data,
+    retrieve_bibnos_for_enhancement,
+)
 
 
 def test_record_export_file_data(init_dataset):
     session = init_dataset
-    fh = "tests/files/bpl-ebk-export-sample.txt"
+    fh = "tests\\files\\bpl-ere-export-sample.txt"
     rec = record_export_file_data(fh, session)
     assert rec.efid == 1
-    assert rec.handle == "bpl-ebk-export-sample.txt"
+    assert rec.handle == "bpl-ere-export-sample.txt"
 
 
 def test_import_sierra_data(init_dataset):
     session = init_dataset
-    import_sierra_data("tests/files/bpl-ebk-export-sample.txt", session)
+    import_sierra_data("tests\\files\\bpl-ere-export-sample.txt", session)
 
     assert len(session.query(Resource).all()) == 4
     assert len(session.query(ExportFile).all()) == 1
+
+
+@pytest.mark.parametrize(
+    "lib_sys,bib_cat,expectation",
+    [
+        ("nyp", "ere", [12345678, 12345679]),
+        ("nyp", "pre", [12345670, 12345671]),
+        ("bpl", "ere", [22345678, 22345679]),
+        ("bpl", "pre", [22345670, 22345671]),
+    ],
+)
+def test_retrieve_bibnos_for_enhancement(
+    lib_sys, bib_cat, expectation, brief_bib_dataset
+):
+    session = brief_bib_dataset
+    assert retrieve_bibnos_for_enhancement(lib_sys, bib_cat, session) == expectation
