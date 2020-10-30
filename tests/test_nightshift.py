f7e585ced07ee87b98efdd99406ac1b1ba92d154
klinga <klingaroo@gmail.com>
2020-10-30 08:45:06 -0400

    pkg title

--- tests/test_nightshift.py
+++ tests/test_nightshift.py
@@ -1,5 +1,9 @@
-from nightshift import __version__
+from nightshift import __version__, __title__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_title():
+    assert __title__ == "NightShift"
