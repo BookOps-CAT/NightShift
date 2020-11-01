f10136daf76ae87d70943d1669207843a770ed89
klinga <klingaroo@gmail.com>
2020-11-01 01:37:48 -0500

    PlatformResponseReader

--- nightshift/api_nyp.py
+++ nightshift/api_nyp.py
@@ -3,23 +3,244 @@
 """
 This module provides methods for the bot to communicate with NYPL Platform (Sierra)
 """
-import json
+from collections import namedtuple
+import datetime
 import os
-from typing import List, Type
+from typing import Dict, List, Tuple, Type
 
-from requests import Response
+import requests
 from bookops_nypl_platform import PlatformToken, PlatformSession
 from bookops_nypl_platform.errors import BookopsPlatformError
 
 
 from . import __version__, __title__
 from .errors import NightShiftError
+from .datastore_values import UPGRADE_SRC
+
+
+SierraMeta = namedtuple(
+    "SierraMeta",
+    [
+        "sbn",
+        "lcn",
+        "did",
+        "sid",
+        "wcn",
+        "title",
+        "author",
+        "pubDate",
+        "upgradeStamp",
+        "upgraded",
+        "upgradeSourceId",
+    ],
+)
 
 
 class PlatformResponseReader:
-    def __init__(self, response: Type[Response]):
+    def __init__(self, responses: Type[requests.Response]):
 
-        self.response = response.json()
+        if responses.status_code == requests.codes.ok:
+            self.datas = responses.json()["data"]
+        elif responses.status_code == 404:
+            self.datas = []
+        else:
+            raise NightShiftError(
+                f"Platform {responses.status_code} error: {responses.json()}"
+            )
+
+    def _get_variable_field_content(self, tag, subfield, data: Dict) -> str:
+        """
+        Generator. Parses variable field subfield $a for specified MARC tag
+
+        Args:
+            tag:                MARC tag
+            data:           single platform record
+
+        Returns:
+            content
+        """
+        content = []
+        for field in data["varFields"]:
+            if field["marcTag"] == tag:
+                for sub in field["subfields"]:
+                    if sub["tag"] == subfield:
+                        content.append(sub["content"].strip())
+        return content
+
+    def _is_upgraded(self, control_number: str) -> Tuple:
+        wcn = self._parse_worldcat_number(control_number)
+        if wcn:
+            upgradeStamp = datetime.datetime.now()
+            upgraded = True
+            upgradeSourceId = UPGRADE_SRC["staff"]["usid"]
+        else:
+            upgradeStamp = None
+            upgraded = False
+            upgradeSourceId = None
+
+        return (wcn, upgradeStamp, upgraded, upgradeSourceId)
+
+    def _map_data(self, data: Dict) -> Type[namedtuple]:
+        sbn = self._parse_isbns(data)
+        lcn = self._parse_lccn(data)
+        did = self._parse_distributor_number(data)
+        sid = self._parse_standard_numbers(data)
+        title = self._parse_title(data)
+        author = self._parse_author(data)
+        pubDate = self._parse_publication_date(data)
+
+        # determine if record in Sierra upgraded manually
+        cno = self._parse_control_number(data)
+        wcn, upgradeStamp, upgraded, upgradeSourceId = self._is_upgraded(cno)
+
+        return SierraMeta(
+            sbn,
+            lcn,
+            did,
+            sid,
+            wcn,
+            title,
+            author,
+            pubDate,
+            upgradeStamp,
+            upgraded,
+            upgradeSourceId,
+        )
+
+    def _parse_author(self, data: Dict) -> str:
+        """
+        Parses normalized author
+
+        Args:
+            data:           single platform record
+
+        Returns:
+            author
+        """
+        return data["normAuthor"]
+
+    def _parse_control_number(self, data: Dict) -> str:
+        """
+        Parses Sierra control number (tag 001)
+
+        Args:
+            data:           single platform record
+
+        Returns:
+            cno
+        """
+        return data["controlNumber"]
+
+    def _parse_distributor_number(self, data: Dict) -> str:
+        """
+        Parses distributor number (tag 037$a - repeatable but enforce one
+        per record)
+
+        Args:
+            data:           single platform record
+
+        Returns:
+            did:                first encountered distributor number
+        """
+        did = self._get_variable_field_content("037", "a", data)
+        if did:
+            return did[0]
+        else:
+            return None
+
+    def _parse_isbns(self, data: Dict) -> str:
+        """
+        Parses ISBN (tag 020)
+
+        Args:
+            data:           single platform record
+
+        Returns:
+            sbn
+        """
+        isbns = self._get_variable_field_content("020", "a", data)
+        if isbns:
+            return ",".join(isbns)
+        else:
+            return None
+
+    def _parse_lccn(self, data: Dict) -> str:
+        """
+        Parses LCCN from data (tag 010 - is not repeatable)
+
+        Args:
+            data:           single platform record
+
+        Returns:
+            lcn
+        """
+        lccn = self._get_variable_field_content("010", "a", data)
+        if lccn:
+            return lccn[0]
+        else:
+            return None
+
+    def _parse_publication_date(self, data: Dict) -> str:
+        """
+        Parses publication date
+        """
+        return str(data["publishYear"])
+
+    def _parse_standard_numbers(self, data: Dict) -> str:
+        """
+        Parses standard number (tag 024$a)
+
+        Args:
+            data:           single platform record
+
+        Returns:
+            sid
+        """
+        sid = self._get_variable_field_content("024", "a", data)
+        if sid:
+            return ",".join(sid)
+        else:
+            return None
+
+    def _parse_title(self, data: Dict) -> str:
+        """
+        Parses normalized title (tag 245 without subfield $c)
+
+        Args:
+            data:           single platform record
+
+        Returns:
+            title
+        """
+        return data["normTitle"]
+
+    def _parse_worldcat_number(self, control_number: str) -> str:
+        """
+        Determines if control number is OCLC/Worldcat number and if
+        so it returns it as pure digits number
+
+        Args:
+            data:           single platform record
+
+        Returns:
+            wcn
+        """
+
+        # NYPL format
+        if control_number.isdigit():
+            return control_number
+        # BPL format
+        elif control_number[0:3] in ["ocm", "ocn"]:
+            return control_number[3:]
+        elif "on" == control_number[:2]:
+            return control_number[2:]
+        else:
+            return None
+
+    def __iter__(self):
+        for data in self.datas:
+            record = self._map_data(data)
+            yield record
 
 
 def split_into_batches(sbids: List[int], size: int = 50) -> List[List]:
@@ -77,6 +298,10 @@ def get_bibs(sbids: List[int]):
         for batch in batches:
             try:
                 # include data if bib deleted
-                response = session.get_bib_list(batch, deleted=None)
+                responses = session.get_bib_list(batch, deleted=None)
             except BookopsPlatformError as exc:
                 raise NightShiftError(f"{exc}")
+
+            reader = PlatformResponseReader(responses)
+            for sierra_meta in reader:
+                pass
--- tests/test_api_nyp.py
+++ tests/test_api_nyp.py
@@ -3,10 +3,12 @@
 """
 Tests bot's NYPL Platform request methods
 """
+import datetime
 
 import pytest
 
-from nightshift.api_nyp import split_into_batches
+from nightshift.api_nyp import split_into_batches, PlatformResponseReader
+from nightshift.errors import NightShiftError
 
 
 @pytest.mark.parametrize(
@@ -32,3 +34,165 @@ def test_split_into_batches(arg, size, expectation):
 
 def test_split_into_batches_default_size():
     assert split_into_batches([1] * 125) == [[1] * 50, [1] * 50, [1] * 25]
+
+
+class TestPlatformResponseReader:
+    """Tests Platorm respnses reader"""
+
+    def test_successful_response(
+        self, stub_nyp_platform_200_response, stub_nyp_responses
+    ):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader.datas == stub_nyp_responses["data"]
+
+    def test_404_response(self, stub_nyp_platform_404_response):
+        reader = PlatformResponseReader(stub_nyp_platform_404_response)
+        assert reader.datas == []
+
+    def test_other_error_response(self, stub_nyp_platform_401_response):
+        err_msg = "Platform 401 error: {'statusCode': 401, 'type': 'unauthorized', 'message': 'Unauthorized'}"
+        with pytest.raises(NightShiftError) as exc:
+            PlatformResponseReader(stub_nyp_platform_401_response)
+            assert err_msg in str(exc.value)
+
+    @pytest.mark.parametrize(
+        "tag,sub,expectation",
+        [
+            ("084", "a", ["BIO007000", "HIS036050", "LIT004020"]),
+            ("037", "a", ["40CC3B3F-4C30-4685-B391-DB7B2EA91455"]),
+            ("856", "3", ["Excerpt", "Image", "Thumbnail"]),
+            ("020", "a", ["9781631491719", "1631491719"]),
+            ("010", "a", ["2017022370"]),
+        ],
+    )
+    def test_get_variable_field_content(
+        self, tag, sub, expectation, stub_nyp_platform_200_response
+    ):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert (
+            reader._get_variable_field_content(tag, sub, reader.datas[0]) == expectation
+        )
+
+    @pytest.mark.parametrize(
+        "arg,expectation",
+        [
+            ("bt123456", (None, None, False, None)),
+            (
+                "ocm00012345",
+                ("00012345", datetime.datetime(2019, 1, 1, 17, 0, 0), True, 2),
+            ),
+        ],
+    )
+    def test_is_upgraded(
+        self, arg, expectation, stub_nyp_platform_200_response, mock_datetime_now
+    ):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._is_upgraded(arg) == expectation
+
+    def test_parse_isbns(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_isbns(reader.datas[0]) == "9781631491719,1631491719"
+
+    def test_parse_isbns_no_field(
+        self, stub_nyp_platform_200_response, stub_platform_record_missing
+    ):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_isbns(stub_platform_record_missing) is None
+
+    def test_parse_control_number(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_control_number(reader.datas[0]) == "ODN0005077214"
+
+    def test_parse_lccn(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_lccn(reader.datas[0]) == "2017022370"
+
+    def test_parses_lccn_missing(
+        self, stub_nyp_platform_200_response, stub_platform_record_missing
+    ):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_lccn(stub_platform_record_missing) is None
+
+    def test_parse_distributor_number(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert (
+            reader._parse_distributor_number(reader.datas[0])
+            == "40CC3B3F-4C30-4685-B391-DB7B2EA91455"
+        )
+
+    def test_parse_distributor_number_missing(
+        self, stub_nyp_platform_200_response, stub_platform_record_missing
+    ):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_distributor_number(stub_platform_record_missing) is None
+
+    def test_parse_standard_nubers(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert (
+            reader._parse_standard_numbers(reader.datas[0]) == "1111111111,2222222222"
+        )
+
+    def test_parse_standard_nubers_missing(
+        self, stub_nyp_platform_200_response, stub_platform_record_missing
+    ):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_standard_numbers(stub_platform_record_missing) is None
+
+    @pytest.mark.parametrize(
+        "arg,expectation",
+        [
+            ("bt123456", None),
+            ("123456789", "123456789"),
+            ("ocm00012345", "00012345"),
+            ("ocn00012345", "00012345"),
+            ("on123456789", "123456789"),
+        ],
+    )
+    def test_parse_worldcat_number(
+        self, arg, expectation, stub_nyp_platform_200_response
+    ):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_worldcat_number(arg) == expectation
+
+    def test_parse_title(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert (
+            reader._parse_title(reader.datas[0])
+            == "saddest words electronic resource william faulkners civil war"
+        )
+
+    def test_parse_author(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_author(reader.datas[0]) == "gorra michael"
+
+    def test_parse_publication_date(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        assert reader._parse_publication_date(reader.datas[0]) == "2020"
+
+    def test_map_data(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        meta = reader._map_data(reader.datas[0])
+        assert meta.sbn == "9781631491719,1631491719"
+        assert meta.lcn == "2017022370"
+        assert meta.did == "40CC3B3F-4C30-4685-B391-DB7B2EA91455"
+        assert meta.sid == "1111111111,2222222222"
+        assert meta.wcn is None
+        assert (
+            meta.title
+            == "saddest words electronic resource william faulkners civil war"
+        )
+        assert meta.author == "gorra michael"
+        assert meta.pubDate == "2020"
+        assert meta.upgradeStamp is None
+        assert meta.upgraded is False
+        assert meta.upgradeSourceId is None
+
+    def test_generator(self, stub_nyp_platform_200_response):
+        reader = PlatformResponseReader(stub_nyp_platform_200_response)
+        loop = 1
+        for record in reader:
+            if loop == 1:
+                assert record.did == "40CC3B3F-4C30-4685-B391-DB7B2EA91455"
+            elif loop == 2:
+                assert record.did == "4E7547BF-6D42-43F4-9180-8FCF302497F3"
+            loop += 1
