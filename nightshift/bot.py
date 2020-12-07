cdb6dc5eb0e25ba86b3bdb309e4ad7b0d5c8c7e9
klinga <klingaroo@gmail.com>
2020-12-07 00:20:45 -0500

    init

---
+++ nightshift/bot.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+This is top level module of the Nighshift application that initiates processsing
+of records
+"""
+
+
+def run():
+    """Launches the bot"""
+
+    # retrieve data files from FTP
+
+    # import data to datastore
+
+    # enhance data
+
+    # import enhanced data to datastore
+
+    # run queries
+
+    # store results info & create marc files
+
+    pass
