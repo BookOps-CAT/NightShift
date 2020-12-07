dfdc83b9cc048a2557422b6291f605b423a6247b
klinga <klingaroo@gmail.com>
2020-12-06 23:54:55 -0500

    type hints

--- nightshift/api_bpl.py
+++ nightshift/api_bpl.py
@@ -6,10 +6,11 @@ This module provides methods for the bot to communicate with BPL Solr platform (
 from collections import namedtuple
 import datetime
 import os
-from typing import Dict, List, Type, Tuple
+from typing import Any, Dict, Iterator, List, Optional, Tuple
 
 from bookops_bpl_solr.session import SolrSession, BookopsSolrError
 import requests
+from requests import Response
 
 from . import __title__, __version__
 from .errors import NightShiftError
@@ -38,7 +39,7 @@ RESPONSE_FIELDS = ",".join(
 
 
 class SolrResponseReader:
-    def __init__(self, response: Type[requests.Response]):
+    def __init__(self, response: Response) -> None:
         if response.status_code == requests.codes.ok:
             try:
                 self.data = response.json()["response"]["docs"][0]
@@ -54,7 +55,7 @@ class SolrResponseReader:
         else:
             self.meta = None
 
-    def _construct_thumbnail_url(self, url: str) -> str:
+    def _construct_thumbnail_url(self, url: str) -> Optional[str]:
         """Based on a cover image url constructs url for the thumbnail"""
         try:
             idx = url.index("ImageType-100")
@@ -62,7 +63,7 @@ class SolrResponseReader:
         except ValueError:
             return None
 
-    def _is_deleted(self, data: Dict) -> bool:
+    def _is_deleted(self, data: Dict[str, Any]) -> bool:
         """
         Parses deleted status of the bib
 
@@ -74,7 +75,9 @@ class SolrResponseReader:
         """
         return data["deleted"]
 
-    def _is_upgraded(self, control_number: str) -> Tuple:
+    def _is_upgraded(
+        self, control_number: str
+    ) -> Tuple[Optional[str], Optional[datetime.datetime], bool, Optional[int]]:
         wcn = self._parse_worldcat_number(control_number)
         if wcn:
             upgradeStamp = datetime.datetime.now()
@@ -87,7 +90,7 @@ class SolrResponseReader:
 
         return (wcn, upgradeStamp, upgraded, upgradeSourceId)
 
-    def _map_data(self, data: Dict) -> Type[namedtuple]:
+    def _map_data(self, data: Dict[str, Any]) -> namedtuple:
         """
         Maps response data into SierraMeta namedtuple
 
@@ -130,61 +133,60 @@ class SolrResponseReader:
             urls,
         )
 
-    def _parse_author(self, data: Dict) -> str:
+    def _parse_author(self, data: Dict[str, Any]) -> Optional[str]:
         """Parses author from the response"""
         try:
             return data["author_raw"][:50].lower().strip()
         except KeyError:
             return None
 
-    def _parse_bib_id(self, data: Dict) -> int:
+    def _parse_bib_id(self, data: Dict[str, Any]) -> int:
         """Parses Sierra bib number from the response"""
         return int(data["id"])
 
-    def _parse_control_number(self, data: Dict) -> str:
+    def _parse_control_number(self, data: Dict[str, Any]) -> Optional[str]:
         """Parses control number (001 tag) from the response"""
         try:
             return data["ss_marc_tag_001"]
         except KeyError:
             return None
 
-    def _parse_distributor_number(self, data: Dict) -> str:
+    def _parse_distributor_number(self, data: Dict[str, Any]) -> Optional[str]:
         """Parses distributor number (037$a tag) from the response"""
         try:
             return data["econtrolnumber"]
         except KeyError:
             return None
 
-    def _parse_isbns(self, data: Dict) -> str:
+    def _parse_isbns(self, data: Dict[str, Any]) -> Optional[str]:
         """Parses ISBNs from the response"""
         try:
             return ",".join([i for i in data["isbn"] if "-" not in i])
         except KeyError:
             return None
 
-    def _parse_lccn(self, data: Dict) -> str:
+    def _parse_lccn(self, data: Dict[str, Any]) -> Optional[str]:
         """Parses LCCN (010 tag) from the response"""
         try:
             return data["ss_marc_tag_010_a"].strip()
         except KeyError:
             return None
 
-    def _parse_publication_date(self, data: Dict) -> str:
+    def _parse_publication_date(self, data: Dict[str, Any]) -> Optional[str]:
         """Parses publication date from the response"""
         try:
             return str(data["publishYear"])[:10].strip()
         except KeyError:
             return None
 
-    def _parse_title(self, data: Dict) -> str:
+    def _parse_title(self, data: Dict[str, Any]) -> str:
         """Parses title from the response"""
         return data["title"][:50].lower().strip()
 
-    def _parse_sierra_format_id(self, data: Dict) -> int:
+    def _parse_sierra_format_id(self, data: Dict[str, Any]) -> int:
         """Parses bib's sierra format"""
         try:
             mat_type = data["material_type"].lower()
-            print(mat_type)
         except (KeyError, TypeError):
             mat_type = "unknown"
 
@@ -199,14 +201,14 @@ class SolrResponseReader:
         else:
             return SIERRA_FORMAT["unknown"]["sfid"]
 
-    def _parse_standard_numbers(self, data: Dict) -> str:
+    def _parse_standard_numbers(self, data: Dict[str, Any]) -> Optional[str]:
         """Parses other standard numbers (024$a tag)"""
         try:
             return ",".join(data["sm_marc_tag_024_a"])
         except KeyError:
             return None
 
-    def _parse_urls(self, data: Dict) -> List[Dict]:
+    def _parse_urls(self, data: Dict) -> List[Dict[int, str]]:
         """Parses urls from the response"""
         urls = []
         if "eurl" in data:
@@ -234,7 +236,7 @@ class SolrResponseReader:
 
         return urls
 
-    def _parse_worldcat_number(self, control_number: str) -> str:
+    def _parse_worldcat_number(self, control_number: str) -> Optional[str]:
         """
         Determines if control number is OCLC/Worldcat number and if
         so it returns it as pure digits number
@@ -258,7 +260,7 @@ class SolrResponseReader:
             return None
 
 
-def get_bpl_sierra_bib_data(sbids: List[int]) -> Type[namedtuple]:
+def get_bpl_sierra_bib_data(sbids: List[int]) -> Iterator[namedtuple]:
     """
     Queries BPL Solr by Sierra bib number
 
