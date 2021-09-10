167ca9380d3d3157d6fc338dd08fc1b220937fa1
Tomek <klingaroo@gmail.com>
2021-09-10 15:18:28 -0400

    tables

---
+++ nightshift/datastore.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+
+"""
+NightShift's database schema.
+
+
+dialect+driver://username:password@host:port/database
+# psycopg2
+engine = create_engine('postgresql+psycopg2://scott:tiger@localhost/mydatabase', client_encoding='utf8')
+"""
+from datetime import datetime
+
+from sqlalchemy import (
+    Boolean,
+    Column,
+    Date,
+    DateTime,
+    ForeignKey,
+    Integer,
+    PickleType,
+    PrimaryKeyConstraint,
+    String,
+    UniqueConstraint,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class Library(Base):
+    """
+    Library system.
+    """
+
+    __tablename__ = "library"
+
+    nid = Column(Integer, primary_key=True)
+    code = Column(String(3), unique=True)
+
+    def __repr__(self):
+        return f"<Library(nid='{self.nid}', code='{self.code}')>"
+
+
+class OutputFile(Base):
+    """
+    Output MARC file info.
+    """
+
+    __tablename__ = "output_file"
+    __table_args__ = (UniqueConstraint("handle", "libraryId"),)
+
+    nid = Column(Integer, primary_key=True)
+    libraryId = Column(Integer, ForeignKey("library.nid"), nullable=False)
+    handle = Column(String, nullable=False)
+    timestamp = Column(DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return (
+            f"<OutputFile(nid='{self.nid}', "
+            f"libraryId='{self.libraryId}', "
+            f"handle='{self.handle}', "
+            f"timestamp='{self.timestamp}')>"
+        )
+
+
+class Resource(Base):
+    """
+    Resource to be upgraded info.
+    """
+
+    __tablename__ = "resource"
+    __table_args__ = (
+        PrimaryKeyConstraint("sierraId", "libraryId"),
+        {},
+    )
+
+    sierraId = Column(Integer, nullable=False)
+    libraryId = Column(Integer, ForeignKey("library.nid"), nullable=False)
+    resourceCategoryId = Column(
+        Integer, ForeignKey("resource_category.nid"), nullable=False
+    )
+
+    archived = Column(Boolean, default=False)
+    bibDate = Column(Date)
+    author = Column(String(100, collation="uft8"))
+    title = Column(String(100, collation="utf8"))
+    pubDate = Column(String)
+
+    congressNumber = Column(String)
+    controlNumber = Column(String)
+    distributorNumber = Column(String)
+    otherNumber = Column(String)
+    outputId = Column(Integer, ForeignKey("output_file.nid"))
+    sourceId = Column(Integer, ForeignKey("source_file.nid"), nullable=False)
+    srcFieldsToKeep = Column(PickleType)
+    standardNumber = Column(String)
+    statusId = Column(Integer, ForeignKey("status.nid"), nullable=False)
+
+    oclcMatchNumber = Column(Integer)
+    upgradeTimestamp = Column(DateTime)
+
+    queries = relationship("WorldcatQuery", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return (
+            f"<Resource(sierraId='{self.sierraId}', libraryId='{self.libraryId}', "
+            f"sourceId='{self.sourceId}', "
+            f"resourceCategoryId='{self.resourceCategoryId}', "
+            f"archived='{self.archived}', "
+            f"bibDate='{self.bibDate}', "
+            f"author='{self.author}', "
+            f"title='{self.title}', "
+            f"pubDate='{self.pubDate}', "
+            f"controlNumber='{self.controlNumber}', "
+            f"congressNumber='{self.congressNumber}', "
+            f"standardNumber='{self.standardNumber}', "
+            f"distributorNumber='{self.distributorNumber}', "
+            f"statusId='{self.statusId}', "
+            f"outputId='{self.outputId}', "
+            f"oclcMatchNumber='{self.oclcMatchNumber}', "
+            f"upgradeTimestamp='{self.upgradeTimestamp}')>"
+        )
+
+
+class ResourceCategory(Base):
+    """
+    Resource Category. Example: ebook, fiction-gen, fiction-mystery.
+    """
+
+    __tablename__ = "resource_category"
+
+    nid = Column(Integer, primary_key=True)
+    code = Column(String, unique=True)
+    description = Column(String)
+
+    def __repr__(self):
+        return (
+            f"<ResourceCategory(nid='{self.nid}', "
+            f"code='{self.code}', "
+            f"description='{self.description}')>"
+        )
+
+
+class SourceFile(Base):
+    """
+    Source MARC file info.
+    """
+
+    __tablename__ = "source_file"
+    __table_args__ = (UniqueConstraint("handle", "libraryId"),)
+
+    nid = Column(Integer, primary_key=True)
+    libraryId = Column(Integer, ForeignKey("library.nid"), nullable=False)
+    handle = Column(String, nullable=False)
+    timestamp = Column(DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return (
+            f"<SourceFile(nid='{self.nid}', "
+            f"libraryId='{self.libraryId}', "
+            f"handle='{self.handle}', "
+            f"timestamp='{self.timestamp}')>"
+        )
+
+
+class Status(Base):
+    """
+    Stores different resource statuses
+    option examples: open, deleted, updated-bot, updated-staff
+    """
+
+    __tablename__ = "status"
+
+    nid = Column(Integer, primary_key=True)
+    name = Column(String(20), unique=True)
+    description = Column(String)
+
+    def __repr__(self):
+        return f"<Status(nid='{self.nid}', name='{self.name}', description='{self.description}')>"
+
+
+class WorldcatQuery(Base):
+    """
+    Metadata API responses.
+    """
+
+    __tablename__ = "worldcat_query"
+
+    nid = Column(Integer, primary_key=True)
+    resourceId = Column(Integer, ForeignKey("resource.sierraId"), nullable=False)
+    libraryId = Column(Integer, ForeignKey("library.nid"), nullable=False)
+    match = Column(Boolean, nullable=False)
+    responseCode = Column(Integer)
+    response = Column(PickleType)  # save as requests.Response object?
+
+    def __repr__(self):
+        return (
+            f"<WorldcatQuery(nid='{self.nid}', "
+            f"resouceId='{self.resourceId}', "
+            f"libraryId='{self.libraryId}', "
+            f"match='{self.match}', ",
+            f"responseCode='{self.responseCode}')>",
+        )
---
+++ tests/test_datastore.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+from nightshift.datastore import (
+    SourceFile,
+    Library,
+    OutputFile,
+    Resource,
+    ResourceCategory,
+)
+
+
+def test_Library_tbl_repr():
+    assert str(Library(nid=1, code="foo")) == "<Library(nid='1', code='foo')>"
+
+
+def test_OutputFile_tbl_repr():
+    stamp = datetime.now()
+    assert (
+        str(OutputFile(nid=1, libraryId=2, handle="foo.mrc", timestamp=stamp))
+        == f"<OutputFile(nid='1', libraryId='2', handle='foo.mrc', timestamp='{stamp}')>"
+    )
+
+
+def test_Resource_tbl_repr():
+    stamp = datetime.now()
+    bibDate = stamp.date()
+    assert (
+        str(
+            Resource(
+                sierraId=1,
+                libraryId=2,
+                resourceCategoryId=3,
+                archived=False,
+                bibDate=bibDate,
+                author="foo",
+                title="spam",
+                pubDate="2021",
+                congressNumber="0001",
+                controlNumber="0002",
+                distributorNumber="0003",
+                otherNumber="0004",
+                outputId=None,
+                sourceId=5,
+                srcFieldsToKeep=None,
+                standardNumber="0005",
+                statusId=6,
+                oclcMatchNumber=None,
+                upgradeTimestamp=stamp,
+            )
+        )
+        == f"<Resource(sierraId='1', libraryId='2', sourceId='5', resourceCategoryId='3', archived='False', bibDate='{bibDate}', author='foo', title='spam', pubDate='2021', controlNumber='0002', congressNumber='0001', standardNumber='0005', distributorNumber='0003', statusId='6', outputId='None', oclcMatchNumber='None', upgradeTimestamp='{stamp}')>"
+    )
+
+
+def test_ResourceCategory_tbl_repr():
+    assert (
+        str(ResourceCategory(nid=1, code="foo", description="spam"))
+        == "<ResourceCategory(nid='1', code='foo', description='spam')>"
+    )
+
+
+def test_SourceFile_tbl_repr():
+    stamp = datetime.now()
+    assert (
+        str(SourceFile(nid=1, libraryId=2, handle="foo.mrc", timestamp=stamp))
+        == f"<SourceFile(nid='1', libraryId='2', handle='foo.mrc', timestamp='{stamp}')>"
+    )
