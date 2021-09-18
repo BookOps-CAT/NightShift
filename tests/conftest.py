3f829e53a888fca6b0d187b0be95d9a61e4c5be4
klinga <klingaroo@gmail.com>
2021-09-17 22:54:51 -0400

    test db rename

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,7 +37,7 @@ def test_engine():
         engine = create_engine(conn)
     elif sys.platform == "linux":
         engine = create_engine(
-            "postgresql+psycopg2://postgres@127.0.0.1:5432/nightshiftTestDB"
+            "postgresql+psycopg2://postgres@127.0.0.1:5432/ns_db"
         )
     else:
         engine = None
