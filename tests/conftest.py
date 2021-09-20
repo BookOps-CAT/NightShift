c60ba328f4412f6f43d9062ad638d8502ce780c7
Tomek <klingaroo@gmail.com>
2021-09-20 09:55:29 -0400

    db conf via env variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,43 +10,64 @@ import yaml
 from nightshift.datastore import Base
 
 
-def db_config():
+def local_test_db_config():
     """
     requires yaml file with local postgres config
 
     example:
     ---
-    host: localhost
-    user:
-    passw:
-    port: 5432
-    name: nightshiftTestDB
+    NS_DBHOST: localhost
+    NS_DBUSER: ns_test
+    NS_DBPASSW: some_password
+    NS_DBPORT: 5432
+    NS_DBNAME: ns_db
     """
     with open("tests/confdatabase.yaml", "r") as f:
         data = yaml.safe_load(f)
         return data
 
 
+@pytest.fixture
+def mock_db_env(monkeypatch):
+    if os.getenv("TRAVIS"):
+        data = dict(
+            user="postgres",
+            passw=None,
+            host="127.0.0.1",
+            port=os.getenv("PGPORT"),
+            name="ns_db",
+        )
+    else:
+        data = local_test_db_config()
+
+    monkeypatch.setenv("NS_DBUSER", data["NS_DBUSER"])
+    monkeypatch.setenv("NS_DBPASSW", data["NS_DBPASSW"])
+    monkeypatch.setenv("NS_DBHOST", data["NS_DBHOST"])
+    monkeypatch.setenv("NS_DBPORT", data["NS_DBPORT"])
+    monkeypatch.setenv("NS_DBNAME", data["NS_DBNAME"])
+
+
 @pytest.fixture(scope="function")
-def test_engine():
+def test_engine(mock_db_env):
     # create db engine differently on local machine or Travis
     if os.getenv("TRAVIS"):
-        engine = create_engine(
-            "postgresql+psycopg2://postgres@127.0.0.1:5433/ns_db")
+        conn = f"postgresql+psycopg2://{os.getenv('NS_DBUSER')}@{os.getenv('NS_DBHOST')}:{os.getenv('NS_PORT')}/{os.getenv('NS_DBNAME')}"
     else:
-        c = db_config()
-        conn = f"postgresql+psycopg2://{c['user']}:{c['passw']}@{c['host']}:{c['port']}/{c['name']}"
-        engine = create_engine(conn)
+        conn = f"postgresql+psycopg2://{os.getenv('NS_DBUSER')}:{os.getenv('NS_DBPASSW')}@{os.getenv('NS_DBHOST')}:{os.getenv('NS_DBPORT')}/{os.getenv('NS_DBNAME')}"
+    engine = create_engine(conn)
     return engine
 
 
 @pytest.fixture(scope="function")
 def test_session(test_engine):
 
+    # setup
     Base.metadata.create_all(test_engine)
     Session = sessionmaker(bind=test_engine)
     session = Session()
 
     yield session
     session.close()
+
+    # teardown
     Base.metadata.drop_all(test_engine)
