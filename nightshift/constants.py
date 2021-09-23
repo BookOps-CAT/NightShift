7a84190b7f5f75ff1c25245ad81aab7766d59cf2
Tomek <klingaroo@gmail.com>
2021-09-23 12:34:57 -0400

    resource categories init

--- nightshift/constants.py
+++ nightshift/constants.py
@@ -2,10 +2,67 @@
 
 LIBRARIES = {"nyp": 1, "bpl": 2}
 RESOURCE_CATEGORIES = {
-    "e_resource": [
-        1,
-        "eresources in general: ebooks, eaudio, evideo",
-        ["020", "037", "856", "901"],
-    ],
-    "print_eng_mono": [2, "English print monograph material", None],
+    "e_resource": {
+        "nid": 1,
+        "description": "Electronic resources (including ebooks, eaudio, evideo)",
+        "src_fields2keep": ["001", "020", "037", "856"],
+        "dst_fields2delete": ["020", "029", "037", "090", "856", "938"],
+        "query_days": [30, 90, 180],
+    },
+    "print_eng_adult_fic": {
+        "nid": 2,
+        "description": "Print English general adult fiction",
+        "src_fields2keep": [],
+        "dst_fields2delete": ["029", "090", "263", "936", "938"],
+        "query_days": [15, 30],
+    },
+    "print_eng_adult_bio": {
+        "nid": 3,
+        "description": "Print English adult biography",
+        "src_fields2keep": [],
+        "dst_fields2delete": ["029", "090", "263", "936", "938"],
+        "query_days": [15, 30],
+    },
+    "print_eng_adult_nonfic": {
+        "nid": 4,
+        "description": "Print English adult non-fiction",
+        "src_fields2keep": [],
+        "dst_fiels2delete": ["029", "090", "263", "936", "938"],
+        "query_days": [15, 30],
+    },
+    "print_eng_adult_mystery": {
+        "nid": 5,
+        "description": "Print English adult mysteries",
+        "src_fields2keep": [],
+        "dst_fields2delete": ["029", "090", "263", "936", "938"],
+        "query_days": [15, 30],
+    },
+    "print_eng_adult_scifi": {
+        "nid": 6,
+        "description": "Print English adult science-fiction",
+        "src_fields2keep": [],
+        "dst_fields2delete": ["029", "090", "263", "936", "938"],
+        "query_days": [15, 30],
+    },
+    "print_eng_juv_fic": {
+        "nid": 7,
+        "description": "Print English juvenile fiction",
+        "src_fields2keep": [],
+        "dst_fields2delete": ["029", "090", "263", "936", "938"],
+        "query_days": [15, 30],
+    },
+    "print_eng_juv_bio": {
+        "nid": 8,
+        "description": "Print English juvenile bio",
+        "src_fields2keep": [],
+        "dst_fields2delete": ["029", "090", "263", "936", "938"],
+        "query_days": [15, 30],
+    },
+    "print_eng_juv_nonfic": {
+        "nid": 9,
+        "description": "Print English juvenile fiction",
+        "src_fields2keep": [],
+        "dst_fields2delete": ["029", "090", "263", "936", "938"],
+        "query_days": [15, 30],
+    },
 }
