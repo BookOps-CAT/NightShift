76ef20f1cdf6953099e0e4862805495f393577a4
Tomek <klingaroo@gmail.com>
2021-09-23 12:35:17 -0400

    BibReader init

--- tests/test_marc_parser.py
+++ tests/test_marc_parser.py
@@ -3,7 +3,5 @@
 from nightshift.marc_parser import BibReader
 
 
-# def test_temp():
-#     reader = BibReader("tests/nyp-ebook-sample.mrc", "nyp", "e_resource")
-#     for b in reader:
-#         pass
+def test_BibReader_pickle_obj():
+    reader = BibReader("foo.mrc", "nyp", "e_resource")
