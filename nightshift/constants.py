b51008ba0827324e3eb2a8d810c69a34391773d6
Tomek <klingaroo@gmail.com>
2021-09-24 13:35:53 -0400

    print query days and tags2delete shared

--- nightshift/constants.py
+++ nightshift/constants.py
@@ -1,68 +1,72 @@
 # -*- coding: utf-8 -*-
 
 LIBRARIES = {"nyp": {"nid": 1}, "bpl": {"nid": 2}}
+
+PRINT_TAGS_TO_KEEP = ["029", "090", "263", "936", "938"]
+PRINT_QUERY_DAYS = [15, 30]
+
 RESOURCE_CATEGORIES = {
-    "e_resource": {
+    "eresource": {
         "nid": 1,
         "description": "Electronic resources (including ebooks, eaudio, evideo)",
-        "src_fields2keep": ["001", "020", "037", "856"],
-        "dst_fields2delete": ["020", "029", "037", "090", "856", "938"],
+        "src_tags2keep": ["001", "020", "037", "856"],
+        "dst_tags2delete": ["020", "029", "037", "090", "856", "938"],
         "query_days": [30, 90, 180],
     },
     "print_eng_adult_fic": {
         "nid": 2,
         "description": "Print English general adult fiction",
-        "src_fields2keep": [],
-        "dst_fields2delete": ["029", "090", "263", "936", "938"],
-        "query_days": [15, 30],
+        "src_tags2keep": [],
+        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_adult_bio": {
         "nid": 3,
         "description": "Print English adult biography",
-        "src_fields2keep": [],
-        "dst_fields2delete": ["029", "090", "263", "936", "938"],
-        "query_days": [15, 30],
+        "src_tags2keep": [],
+        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_adult_nonfic": {
         "nid": 4,
         "description": "Print English adult non-fiction",
-        "src_fields2keep": [],
-        "dst_fiels2delete": ["029", "090", "263", "936", "938"],
-        "query_days": [15, 30],
+        "src_tags2keep": [],
+        "dst_fiels2delete": PRINT_TAGS_TO_KEEP,
+        "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_adult_mystery": {
         "nid": 5,
         "description": "Print English adult mysteries",
-        "src_fields2keep": [],
-        "dst_fields2delete": ["029", "090", "263", "936", "938"],
-        "query_days": [15, 30],
+        "src_tags2keep": [],
+        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_adult_scifi": {
         "nid": 6,
         "description": "Print English adult science-fiction",
-        "src_fields2keep": [],
-        "dst_fields2delete": ["029", "090", "263", "936", "938"],
-        "query_days": [15, 30],
+        "src_tags2keep": [],
+        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_juv_fic": {
         "nid": 7,
         "description": "Print English juvenile fiction",
-        "src_fields2keep": [],
-        "dst_fields2delete": ["029", "090", "263", "936", "938"],
-        "query_days": [15, 30],
+        "src_tags2keep": [],
+        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_juv_bio": {
         "nid": 8,
         "description": "Print English juvenile bio",
-        "src_fields2keep": [],
-        "dst_fields2delete": ["029", "090", "263", "936", "938"],
-        "query_days": [15, 30],
+        "src_tags2keep": [],
+        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_juv_nonfic": {
         "nid": 9,
         "description": "Print English juvenile fiction",
-        "src_fields2keep": [],
-        "dst_fields2delete": ["029", "090", "263", "936", "938"],
-        "query_days": [15, 30],
+        "src_tags2keep": [],
+        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "query_days": PRINT_QUERY_DAYS,
     },
 }
