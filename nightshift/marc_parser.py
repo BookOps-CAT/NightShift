ad9a4b9968398bbb480edc8db66cf998c639e856
Tomek <klingaroo@gmail.com>
2021-09-24 13:36:57 -0400

    resource category to be determined for each bib separately

--- nightshift/marc_parser.py
+++ nightshift/marc_parser.py
@@ -10,7 +10,7 @@ evideo?)
 """
 # from collections import namedtuple
 import pickle
-from typing import Any, Iterator
+from typing import Any, Iterator, Optional
 
 from pymarc import Field
 from bookops_marc import SierraBibReader, Bib
@@ -26,7 +26,6 @@ class BibReader:
         self,
         marc_fh: str,
         library: str,
-        resource_category: str,
         hide_utf8_warnings: bool = True,
     ) -> None:
         """
@@ -39,15 +38,9 @@ class BibReader:
 
         if library not in LIBRARIES.keys():
             raise NightShiftError("Invalid 'library' argument. Must be 'nyp' or 'bpl'.")
-        if resource_category not in RESOURCE_CATEGORIES.keys():
-            raise NightShiftError(
-                "Invalid 'resource_category' argument. "
-                "See datastore.RESOURCE_CATEGORIES for list of valid values."
-            )
 
         self.marc_fh = marc_fh
         self.library = library
-        self.resource_category = resource_category
         self.hide_utf8_warnings = hide_utf8_warnings
 
     def __iter__(self) -> Iterator[Resource]:
@@ -56,24 +49,51 @@ class BibReader:
                 marcfile, hide_utf8_warnings=self.hide_utf8_warnings
             )
             for bib in reader:
-                bib_info = self._map_data(bib)
-                yield bib_info
+                resource_category = self._determine_resource_category(bib)
+
+                # skip any unmapped resource types from processing
+                if resource_category:
+                    bib_info = self._map_data(bib, resource_category)
+                    yield bib_info
+                else:
+                    # log a warning
+                    pass
+
+    def _determine_resource_category(self, bib: Bib) -> Optional[str]:
+        """
+        Determies resource category based on bib and order information.
+
+        Args:
+            bib:                            `bookops_marc.Bib` instance
+
+        Returns:
+            resource_category
+        """
+        # Overdrive MarcExpress records control number starts with ODN
+        control_number = bib.control_number()
+        if control_number.startswith("ODN"):
+            return "eresource"
+        else:
+            # future hook
+            # determine particular resource category for print material here
+            # based it on order information from the 960/961 tags
+            return None
 
     def _pickle_obj(self, obj: Any) -> bytes:
         return pickle.dumps(obj)
 
-    def _fields2keep(self, bib: Bib) -> list[Field]:
+    def _fields2keep(self, bib: Bib, resource_category: str) -> list[Field]:
         """
         Resource category specific MARC tags to be carried over to output records
         """
         keep = []
-        tags2keep = RESOURCE_CATEGORIES[self.resource_category]["src_fields2keep"]
+        tags2keep = RESOURCE_CATEGORIES[resource_category]["src_tags2keep"]
         keep.extend(bib.get_fields(*tags2keep))
         pickled_keep = self._pickle_obj(keep)
 
         return pickled_keep
 
-    def _map_data(self, bib: Bib) -> Resource:
+    def _map_data(self, bib: Bib, resource_category: str) -> Resource:
         """
         Maps selected data from bib to `datastore.Resource` table columns.
 
@@ -85,7 +105,7 @@ class BibReader:
         """
         sierraId = bib.sierra_bib_id_normalized()
         libraryId = LIBRARIES[self.library]["nid"]
-        resourceCategoryId = RESOURCE_CATEGORIES[self.resource_category]["nid"]
+        resourceCategoryId = RESOURCE_CATEGORIES[resource_category]["nid"]
         bibDate = bib.created_date()
         author = bib.author()
         title = bib.title()
@@ -94,7 +114,7 @@ class BibReader:
         controlNumber = bib.control_number()
         distributorNumber = bib.overdrive_number()
         otherNumber = bib.upc_number()
-        srcFieldsToKeep = self._fields2keep(bib)
+        srcFieldsToKeep = self._fields2keep(bib, resource_category)
         standardNumber = bib.isbn()
 
         return Resource(
--- tests/test_marc_parser.py
+++ tests/test_marc_parser.py
@@ -5,7 +5,7 @@ from nightshift.marc_parser import BibReader
 
 
 def test_BibReader_iterator():
-    reader = BibReader("tests/nyp-ebook-sample.mrc", "nyp", "e_resource")
+    reader = BibReader("tests/nyp-ebook-sample.mrc", "nyp")
     with does_not_raise():
         for bib in reader:
             pass
