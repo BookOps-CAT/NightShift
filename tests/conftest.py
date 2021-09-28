05e1e90221e23ae55c204f793ffc9808036d1eb6
klinga <klingaroo@gmail.com>
2021-09-27 23:53:53 -0400

    add stub_marc

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@
 
 import os
 
+from bookops_marc import Bib
+from pymarc import Field
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -71,3 +73,33 @@ def test_session(test_connection):
 
     # teardown
     Base.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def stub_marc():
+    bib = Bib()
+    bib.leader = "02866pam  2200517 i 4500"
+    bib.add_field(Field(tag="008", data="190306s2017    ht a   j      000 1 hat d"))
+    bib.add_field(
+        Field(
+            tag="100",
+            indicators=["1", " "],
+            subfields=["a", "Adams, John,", "e", "author."],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="245",
+            indicators=["1", "4"],
+            subfields=["a", "The foo /", "c", "by John Adams."],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="264",
+            indicators=[" ", "1"],
+            subfields=["a", "Bar :", "b", "New York,", "c", "2021"],
+        )
+    )
+
+    return bib
