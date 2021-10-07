5d233b2422e2387fc4bb45e109d70f57fff796b3
klinga <klingaroo@gmail.com>
2021-10-07 00:39:50 -0400

    search_batch() resource retrieval

--- nightshift/manager.py
+++ nightshift/manager.py
@@ -4,26 +4,30 @@ This module incldues top level processes to be performed by the app
 from bookops_worldcat.errors import WorldcatSessionError
 
 
+from nightshift.datastore import session_scope
+from nightshfit.datastore_transactions import retrieve_new_resources
 from nightshift.worldcat import search_batch
 
 
-def process_todays_files(library: str):
+def process_new_files():
     # find if any MARC files have been dropped from Sierra to the
     # network drive
     # parse them and save data into the db
 
     # retrieve today's resouces
-    # search today's resources
-    resources = []
 
-    try:
-        results = search_batch(library=library, resources=resources)
-        for res in results:
-            # save results
+    # search today's resources
+    with session_scope() as db_session:
+        resources = retrieve_new_resources(db_session)
+
+        try:
+            results = search_batch(library=library, resources=resources)
+            for res in results:
+                # save results
+                pass
+        except WorldcatSessionError:
+            # log the problem
             pass
-    except WorldcatSessionError:
-        # log the problem
-        pass
 
     # search db for successfuly matched resources
     # produce MARC records
