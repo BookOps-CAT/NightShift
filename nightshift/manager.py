dc41d3664e04e83da240e023b2949cc9d6280ff3
Tomek <klingaroo@gmail.com>
2021-10-13 12:33:22 -0400

    manager get_full_bibs added

--- nightshift/manager.py
+++ nightshift/manager.py
@@ -6,63 +6,80 @@ from bookops_worldcat.errors import WorldcatSessionError
 
 from nightshift.constants import LIBRARIES
 from nightshift.datastore import session_scope
-from nightshfit.datastore_transactions import retrieve_new_resources, update_resource
+from nightshfit.datastore_transactions import (
+    retrieve_matched_resources,
+    retrieve_new_resources,
+    update_resource,
+)
 from nightshift import worldcat
 from nightshift.marc.marc_parser import BibReader
 
 
-def process_new_files():
+def process_new_resources():
     """
-    Processes any newly added files.
+    Processes any newly added resources.
     """
-
-    # find if any MARC files have been dropped from Sierra to the
-    # network drive
-    # parse them and save data into the db
     with session_scope() as db_session:
-        for library, v in LIBRARIES.items():
-            resources = BibReader(marc_fh="tests/nyp-ebook-sample.mrc", library=library)
-            for resource in resources:
-                insert_or_ignore(db_session, Resource, resource)
-                db_session.commit()
-
         # retrieve today's resouces & search WorldCat
-        for library, v in LIBRARIES.items():
-            resources = retrieve_new_resources(db_session, v["nid"])
+        for library, libdata in LIBRARIES.items():
+            resources = retrieve_new_resources(db_session, libdata["nid"])
 
             try:
                 results = worldcat.search_batch(library=library, resources=resources)
                 for resource, response in results:
                     if response is not None:
-                        oclcNumber = parse_oclc_number(response)
+                        oclcNumber = worldcat.get_oclc_number(response)
                         queries = resource.queries.append(
                             resourceId=resource.nid,
                             match=True,
                             response=response.json(),
                         )
-                        res = update_resource(
+                        result = update_resource(
                             db_session,
                             resource.sierraId,
                             resource.libraryId,
                             oclcMatchNumber=oclcNumber,
+                            status="matched",
                             queries=queries,
                         )
+
                     else:
                         queries = resource.queries.append(
                             resourceId=resource.nid, match=False
                         )
-                        res = update_resource(
+                        result = update_resource(
                             db_session,
                             resource.sierraId,
                             resource.libraryId,
                             queries=queries,
                         )
                     db_session.commit()
+                    if not result:
+                        # log the issue
+                        pass
             except WorldcatSessionError:
                 # log the problem
                 pass
 
-    # search db for successfuly matched resources
+            resources = retrieve_matched_resources(db_session, libdata["nid"])
+            for resource in resources:
+                try:
+                    response = worldcat.full_bib_request(resource.oclcMatchNumber)
+                    result = update_resource(
+                        db_session,
+                        resource.sierraId,
+                        resource.libraryId,
+                        fullBib=response.content,
+                    )
+                    if not result:
+                        # log error
+                        pass
+                except WorldcatSessionError:
+                    # log, try another library?
+                    break
+
+            resources = retrieve_upgrade_ready_resources(db_session, libdata["nid"])
+
     # produce MARC records
     # output MARC records to the network drive
     # notify
