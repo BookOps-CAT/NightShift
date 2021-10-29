721a41e2f2ee9d56020b2da3310bf6a0a248abd2
Tomek <klingaroo@gmail.com>
2021-10-29 13:33:36 -0400

    Drive init

--- nightshift/comms/storage.py
+++ nightshift/comms/storage.py
@@ -4,40 +4,64 @@
 This module handles communication with network drive accessible via SFTP where Sierra
 dumps daily files for processing and where CAT staff can access them
 """
+import logging
 import os
 
-import paramiko
 
-paramiko.util.log_to_file("nightshift.log")
+from paramiko.transport import Transport
+from paramiko.sftp_client import SFTPClient
 
 
-class DriveSFTP:
-    def __init__(self):
-        host, user, password, self.home_dir = self._get_credentials()
-        transport = paramiko.Transport((host))
+logger = logging.getLogger("nightshift")
+
+
+def get_credentials():
+    """
+    Retrieves SFTP credentials from environmental variables.
+
+    Returns:
+        credentials
+    """
+    return (
+        os.getenv("SFTP_HOST"),
+        os.getenv("SFTP_USER"),
+        os.getenv("SFTP_PASSW"),
+        os.getenv("SFTP_NS_HOME"),
+    )
+
+
+class Drive:
+    def __init__(self, host, user, password, home_directory):
+        """
+        Opens communication channel via SFTP to networked drive
+
+        Args:
+            host:                       SFTP host
+            user:                       SFTP user name
+            password:                   SFTP user password
+            home_directory:             NighShift directory on the drive
+        """
+        self.sftp = self._sftp(host, user, password, home_directory)
+
+    def list_directory(self):
+        return self.sftp.listdir()
+
+    def _sftp(self, host, user, password, home_directory):
+        transport = Transport((host))
         transport.connect(None, user, password)
-        self.sftp = paramiko.SFTPClient.from_transport(transport)
+        sftp = SFTPClient.from_transport(transport)
+        sftp.chdir(home_directory)  # wrap in an exception
+        return sftp
 
-    def __enter__(self):
-        return self.sftp
+    def __enter__(self, *args):
+        return self
 
-    def __exit__(self):
-        if self.sftp:
-            self.sftp.close()
+    def __exit__(self, *args):
+        self.close()
 
     def close(self):
-        self.sftp.close()
-
-    def _get_credentials(self) -> dict:
         """
-        Retrieves SFTP credentials from environmental variables.
-
-        Returns:
-            credentials
+        Closes SFTP session and underlying channel.
         """
-        return (
-            os.getenv("SFTP_HOST"),
-            os.getenv("SFTP_USER"),
-            os.getenv("SFTP_PASSW"),
-            os.getenv("SFTP_DIR"),
-        )
+        if self.sftp:
+            self.sftp.close()
