966ef8da4e76da5531a34e96db426339b5ad114c
Tomek <klingaroo@gmail.com>
2021-10-29 13:35:24 -0400

    test init

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,3 +1,21 @@
 import os
 
-from nightshift.comms.storage import temp_sftp
+from nightshift.comms.storage import get_credentials, Drive
+
+
+def test_get_credentials(mock_sftp_env):
+    assert get_credentials() == (
+        "sftp_host",
+        "sftp_user",
+        "sftp_password",
+        "nightshift_home_dir",
+    )
+
+
+# class TestLiveDrive:
+#     def test_init(self, live_sftp_env):
+#         creds = get_credentials()
+#         with Drive(*creds) as sftp:
+#             print(type(sftp))
+#             # sftp = Drive.sftp()
+#             print(sftp.list_directory())
