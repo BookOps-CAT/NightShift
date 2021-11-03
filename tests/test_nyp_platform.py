69624430e0f3a5e8bf28142f9d6d189ee1cf61f0
Tomek <klingaroo@gmail.com>
2021-11-03 14:53:33 -0400

    NypPlatform init

---
+++ tests/test_nyp_platform.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from contextlib import nullcontext as does_not_raise
+import logging
+
+from bookops_nypl_platform import PlatformToken
+import pytest
+
+from nightshift import __title__, __version__
+from nightshift.ns_exceptions import SierraSearchPlatformError
+from nightshift.comms.nyp_platform import NypPlatform
+
+
+class TestNypPlatformMocked:
+    def test_successful_initation(
+        self, mock_platform_env, mock_successful_platform_post_token_response
+    ):
+        with does_not_raise():
+            platform = NypPlatform()
+            assert platform.headers == {
+                "User-Agent": f"{__title__}/{__version__}",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept": "application/json",
+                "Connection": "keep-alive",
+                "Authorization": "Bearer token_string_here",
+            }
+            assert isinstance(platform.authorization, PlatformToken)
+
+    def test_failed_authorization(
+        self, caplog, mock_platform_env, mock_failed_platform_post_token_response
+    ):
+        with caplog.at_level(logging.ERROR):
+            with pytest.raises(SierraSearchPlatformError):
+                NypPlatform()
+
+        assert "Unable to obtain access token for NYPL Platform." in caplog.text
