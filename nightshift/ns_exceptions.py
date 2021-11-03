9b3d19330ddc3dd0b5491a729d59fa38fba7d414
Tomek <klingaroo@gmail.com>
2021-11-03 14:52:08 -0400

    added SierraSearchPlatformError

--- nightshift/ns_exceptions.py
+++ nightshift/ns_exceptions.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
 """
-This module contains nighshift exceptions
+This module contains NightShift exceptions
 """
 
 
 class DriveError(Exception):
     """
     Exception raised when SFTP/Drive error is encountered
+    """
+
+    pass
+
+
+class SierraSearchPlatformError(Exception):
+    """
+    Exception raised during sessions with NYPL Platform and BPL Solr
     """
 
     pass
