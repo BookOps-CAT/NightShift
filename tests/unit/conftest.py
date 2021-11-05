559f3f9f5256143004dd56312b19b73b4d8bce0e
Tomek <klingaroo@gmail.com>
2021-11-05 10:33:48 -0400

    removed live_sftp_env

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -346,18 +346,6 @@ def mock_Worldcat(mock_worldcat_creds, mock_successful_post_token_response):
 
 
 @pytest.fixture
-def live_sftp_env(monkeypatch):
-    if not os.getenv("TRAVIS"):
-        with open("tests/envar.yaml", "r") as f:
-            data = yaml.safe_load(f)
-            monkeypatch.setenv("SFTP_HOST", data["SFTP_HOST"])
-            monkeypatch.setenv("SFTP_USER", data["SFTP_USER"])
-            monkeypatch.setenv("SFTP_PASSW", data["SFTP_PASSW"])
-            monkeypatch.setenv("SFTP_NS_SRC", data["SFTP_NS_SRC"])
-            monkeypatch.setenv("SFTP_NS_DST", data["SFTP_NS_DST"])
-
-
-@pytest.fixture
 def mock_sftp_env(monkeypatch, sftpserver):
     monkeypatch.setenv("SFTP_HOST", sftpserver.host)
     monkeypatch.setenv("SFTP_PORT", str(sftpserver.port))
