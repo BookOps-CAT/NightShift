e79dabef4e96d79394a3dcdd96f46f98447d2773
Tomek <klingaroo@gmail.com>
2021-11-05 10:27:36 -0400

    live_sftp_env > env_var & test_db fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,16 +2,30 @@
 import os
 
 import pytest
+from sqlalchemy import create_engine
 import yaml
 
 
+from nightshift.datastore import Base
+from nightshift.datastore_transactions import init_db
+
+
 @pytest.fixture
-def live_sftp_env(monkeypatch):
+def env_var(monkeypatch):
     if not os.getenv("TRAVIS"):
         with open("tests/envar.yaml", "r") as f:
             data = yaml.safe_load(f)
-            monkeypatch.setenv("SFTP_HOST", data["SFTP_HOST"])
-            monkeypatch.setenv("SFTP_USER", data["SFTP_USER"])
-            monkeypatch.setenv("SFTP_PASSW", data["SFTP_PASSW"])
-            monkeypatch.setenv("SFTP_NS_SRC", data["SFTP_NS_SRC"])
-            monkeypatch.setenv("SFTP_NS_DST", data["SFTP_NS_DST"])
+            for k, v in data.items():
+                monkeypatch.setenv(k, v)
+
+
+@pytest.fixture
+def test_db(env_var):
+    # setup
+    init_db()
+    yield
+
+    # teardown
+    test_connection = f"postgresql://{os.getenv('NS_DBUSER')}:{os.getenv('NS_DBPASSW')}@{os.getenv('NS_DBHOST')}:{os.getenv('NS_DBPORT')}/{os.getenv('NS_DBNAME')}"
+    engine = create_engine(test_connection)
+    Base.metadata.drop_all(engine)
