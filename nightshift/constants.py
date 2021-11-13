946d04aa40188ff0b5baadffe15c7d5a29df9ef9
klinga <klingaroo@gmail.com>
2021-11-12 19:15:27 -0500

    fixes: misleading name for tags to delete & 910 tag protection

--- nightshift/constants.py
+++ nightshift/constants.py
@@ -2,7 +2,7 @@
 
 LIBRARIES = {"nyp": {"nid": 1}, "bpl": {"nid": 2}}
 
-PRINT_TAGS_TO_KEEP = ["029", "090", "263", "936", "938"]
+PRINT_TAGS_TO_DELETE = ["029", "090", "263", "936", "938"]
 PRINT_QUERY_DAYS = [(15, 30), (30, 45)]
 
 RESOURCE_CATEGORIES = {
@@ -10,77 +10,77 @@ RESOURCE_CATEGORIES = {
         "nid": 1,
         "description": "digital book",
         "src_tags2keep": ["001", "020", "037", "856"],
-        "dst_tags2delete": ["020", "029", "037", "090", "856", "938"],
+        "dst_tags2delete": ["020", "029", "037", "090", "856", "910", "938"],
         "query_days": [(30, 90), (90, 180)],
     },
     "eaudio": {
         "nid": 2,
         "description": "digital audiobook",
         "src_tags2keep": ["001", "020", "037", "856"],
-        "dst_tags2delete": ["020", "029", "037", "090", "856", "938"],
+        "dst_tags2delete": ["020", "029", "037", "090", "856", "910", "938"],
         "query_days": [(30, 90), (90, 180)],
     },
     "evideo": {
         "nid": 3,
         "description": "video digital",
         "src_tags2keep": ["001", "020", "037", "856"],
-        "dst_tags2delete": ["020", "029", "037", "090", "856", "938"],
+        "dst_tags2delete": ["020", "029", "037", "090", "856", "910", "938"],
         "query_days": [(30, 90)],
     },
     "print_eng_adult_fic": {
         "nid": 4,
         "description": "Print English general adult fiction",
-        "src_tags2keep": [],
-        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "src_tags2keep": ["910"],
+        "dst_tags2delete": PRINT_TAGS_TO_DELETE,
         "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_adult_bio": {
         "nid": 5,
         "description": "Print English adult biography",
-        "src_tags2keep": [],
-        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "src_tags2keep": ["910"],
+        "dst_tags2delete": PRINT_TAGS_TO_DELETE,
         "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_adult_nonfic": {
         "nid": 6,
         "description": "Print English adult non-fiction",
-        "src_tags2keep": [],
-        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "src_tags2keep": ["910"],
+        "dst_tags2delete": PRINT_TAGS_TO_DELETE,
         "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_adult_mystery": {
         "nid": 7,
         "description": "Print English adult mysteries",
-        "src_tags2keep": [],
-        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "src_tags2keep": ["910"],
+        "dst_tags2delete": PRINT_TAGS_TO_DELETE,
         "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_adult_scifi": {
         "nid": 8,
         "description": "Print English adult science-fiction",
-        "src_tags2keep": [],
-        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "src_tags2keep": ["910"],
+        "dst_tags2delete": PRINT_TAGS_TO_DELETE,
         "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_juv_fic": {
         "nid": 9,
         "description": "Print English juvenile fiction",
-        "src_tags2keep": [],
-        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "src_tags2keep": ["910"],
+        "dst_tags2delete": PRINT_TAGS_TO_DELETE,
         "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_juv_bio": {
         "nid": 10,
         "description": "Print English juvenile bio",
-        "src_tags2keep": [],
-        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "src_tags2keep": ["910"],
+        "dst_tags2delete": PRINT_TAGS_TO_DELETE,
         "query_days": PRINT_QUERY_DAYS,
     },
     "print_eng_juv_nonfic": {
         "nid": 11,
         "description": "Print English juvenile fiction",
-        "src_tags2keep": [],
-        "dst_tags2delete": PRINT_TAGS_TO_KEEP,
+        "src_tags2keep": ["910"],
+        "dst_tags2delete": PRINT_TAGS_TO_DELETE,
         "query_days": PRINT_QUERY_DAYS,
     },
 }
--- tests/unit/test_constants.py
+++ tests/unit/test_constants.py
@@ -4,20 +4,20 @@
 from nightshift.constants import library_by_nid, tags2delete
 
 
-def test_tags2delete_by_resource_category_nid_correct_keys():
+def test_tags2delete_correct_keys():
     result = tags2delete()
     assert isinstance(result, dict)
     for key in result.keys():
         assert isinstance(key, int)
 
 
-def test_tags2delete_by_resource_category_nid_correct_values():
+def test_tags2delete_correct_values():
     result = tags2delete()
     values = result.values()
     for v in values:
         assert isinstance(v, list)
 
-    assert result[1] == ["020", "029", "037", "090", "856", "938"]
+    assert result[1] == ["020", "029", "037", "090", "856", "910", "938"]
     assert result[4] == ["029", "090", "263", "936", "938"]
 
 
