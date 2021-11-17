79e148da765fe4d0640a373ffbb9012f8acb7aa1
Tomek <klingaroo@gmail.com>
2021-11-17 08:19:33 -0500

    adds BPL Solr 403 HTTP code handling test

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -504,11 +504,11 @@ class MockPlatformSessionResponseNotFound:
     """Simulates NYPL Platform failed query response"""
 
     def __init__(self):
-        self.status_code = 404
+        self.status_code = 403
 
     def json(self):
         return {
-            "statusCode": 404,
+            "statusCode": 403,
             "type": "exception",
             "message": "No record found",
             "error": [],
