9889e76303d6f24fdcd6dbdac2bf7e92ade11ceb
klinga <klingaroo@gmail.com>
2021-11-22 22:15:25 -0500

    inital workflow

--- nightshift/manager.py
+++ nightshift/manager.py
@@ -3,23 +3,22 @@ This module incldues top level processes to be performed by the app
 """
 import logging
 
-from bookops_worldcat.errors import WorldcatSessionError
-from sqlalchemy.orm.session import Session
 
-from nightshift.comms.worldcat import Worldcat
-from nightshift.constants import LIBRARIES, RESOURCE_CATEGORIES
-from nightshift.datastore import session_scope, Resource, WorldcatQuery
+from nightshift.constants import library_by_id, RESOURCE_CATEGORIES
+from nightshift.datastore import session_scope
 from nightshift.datastore_transactions import (
     retrieve_new_resources,
     retrieve_open_matched_resources_with_full_bib_obtained,
     retrieve_open_matched_resources_without_full_bib,
     retrieve_open_older_resources,
-    update_resource,
 )
 from nightshift.tasks import (
     check_resources_sierra_state,
+    enhance_and_output_bibs,
     get_worldcat_brief_bib_matches,
     get_worldcat_full_bibs,
+    transfer_to_drive,
+    upgrade_status_to_upgraded,
 )
 
 
@@ -34,27 +33,29 @@ def process_resources() -> None:
     """
     with session_scope() as db_session:
 
-        for library, libdata in LIBRARIES.items():
-            logger.info(f"Processing {library} new resources.")
+        for lib_nid, library in library_by_id():
+
+            # ingest new resources if any
+            # here
 
             # search newly added resources
-            resources = retrieve_new_resources(db_session, libdata["nid"])
+            resources = retrieve_new_resources(db_session, lib_nid)
             logger.info(
                 f"Retrieved {len(resources)} new {library} resources for "
                 "querying WorldCat."
             )
-
             # perform searches for each resource and store results
             get_worldcat_brief_bib_matches(db_session, library, resources)
 
             # check & update status of older resources if changed in Sierra
-            for res_category, catdata in RESOURCE_CATEGORIES.items():
-                for ageMin, ageMax in catdata["query_days"]:
+            for res_category, res_cat_data in RESOURCE_CATEGORIES.items():
+                for age_min, age_max in res_cat_data["query_days"]:
                     resources = retrieve_open_older_resources(
                         db_session,
-                        libdata["nid"],
-                        ageMin,
-                        ageMax,
+                        lib_nid,
+                        res_cat_data["nid"],
+                        age_min,
+                        age_max,
                     )
                     logger.info(
                         f"Retrieved {len(resources)} {library} {res_category} "
@@ -63,32 +64,56 @@ def process_resources() -> None:
                     # query Sierra platform to update their status if changed
                     check_resources_sierra_state(db_session, library, resources)
 
+            # search again older resources dropping any resources already cataloged
+            # or deleted
+            for res_category, res_cat_data in RESOURCE_CATEGORIES.items():
+                for ageMin, ageMax in res_cat_data["query_days"]:
+                    resources = retrieve_open_older_resources(
+                        db_session,
+                        lib_nid,
+                        res_cat_data["nid"],
+                        age_min,
+                        age_max,
+                    )
+                    logger.info(
+                        f"Retrieved {len(resources)} {library} {res_category} "
+                        "ready to be queried in WorldCat."
+                    )
 
-#                     # here
-#                     # this will be particulary important for print materials
-#                     pass
+                    # perform WorldCat searches for open older resources
+                    get_worldcat_brief_bib_matches(db_session, library, resources)
 
-#             # search again older resources dropping any resources already cataloged
-#             # or deleted/suppessed
-#             for res_category, catdata in RESOURCE_CATEGORIES.items():
-#                 for ageMin, ageMax in catdata["query_days"]:
-#                     resources = retrieve_older_open_resources(
-#                         db_session,
-#                         libdata["nid"],
-#                         catdata["nid"],
-#                         catdata["query_days"],
-#                     )
+            # perform download of full records for matched resources
+            resources = retrieve_open_matched_resources_without_full_bib(
+                db_session, lib_nid
+            )
+            logger.info(
+                f"Retrieved {len(resources)} {library} {res_category} resources ready "
+                "for downloading full WorldCat records."
+            )
+            get_worldcat_full_bibs(db_session, library, resources)
 
-#                 # perform download of full records for matched resources
-#                 resources = retrieve_matched_resources(db_session, libdata["nid"])
-#                 get_worldcat_full_bibs(db_session, library, resources)
+            # serialize as MARC21 and output to a file of enhanced bibs
+            for res_category, res_cat_data in RESOURCE_CATEGORIES.items():
+                resources = retrieve_open_matched_resources_with_full_bib_obtained(
+                    db_session, library, res_cat_data["nid"]
+                )
 
-#                 # serialize as MARC21 and output to a file of enhanced bibs
-#                 resources = retrieve_full_bib_resources(db_session, library)
-#                 ...
+                # manipulate Worldcat bibs
+                enhance_and_output_bibs(resources)
+                logger.info(
+                    f"Enhancing {len(resources)} {library} {res_category} records."
+                )
 
-#                 # output MARC records to the network drive
-#                 # notify (loggly?)
+                # output MARC records to the network drive
+                transfer_to_drive(library, res_category)
 
-#         # perform db maintenance
-#         # mark resources as expired
+                # notify (loggly?)
+
+                # update resources as upgraded
+                upgrade_status_to_upgraded(db_session, resources)
+
+
+def perform_db_maintenance():
+    pass
+    # mark resources as expired
