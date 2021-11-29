21c86a56cac6b38d78c46c03ea66d6c25d37856b
Tomek <klingaroo@gmail.com>
2021-11-29 14:16:40 -0500

    init test

--- tests/integration/test_manager.py
+++ tests/integration/test_manager.py
@@ -1,6 +1,13 @@
+from contextlib import nullcontext as does_not_raise
+from datetime import datetime
+
 import pytest
+
+
+from nightshift.manager import process_resources
 
 
 @pytest.mark.firewalled
 def test_process_resources(env_var, test_data):
-    pass
+    with does_not_raise():
+        process_resources()
