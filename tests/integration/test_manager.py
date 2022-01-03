e0e613367e66c36dfe60f882ace182159a2f8750
Tomek <klingaroo@gmail.com>
2022-01-03 14:37:22 -0500

    comments out call counters

--- tests/integration/test_manager.py
+++ tests/integration/test_manager.py
@@ -2,7 +2,7 @@ from contextlib import nullcontext as does_not_raise
 from datetime import datetime
 
 import pytest
-
+from pytest_sftpserver.sftp.server import SFTPServer
 
 from nightshift.manager import process_resources
 
@@ -11,3 +11,18 @@ from nightshift.manager import process_resources
 def test_process_resources(env_var, test_data):
     with does_not_raise():
         process_resources()
+
+
+# def calls_counter(func):
+#     def wrapper(*args, **kwargs):
+#         wrapper.count += 1
+#         return func(*args, **kwargs)
+
+#     wrapper.count = 0
+#     return wrapper
+
+
+# @calls_counter
+# def sftpserver_multi_call(sftpserver):
+#     print(f"sftpserver call #: {sftpserver_multi_call.count}")
+#     return sftpserver.serve_content({})
