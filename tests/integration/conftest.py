5529a2ce25d597d19be80f17022f5f893be4caba
Tomek <klingaroo@gmail.com>
2022-01-05 14:44:32 -0500

    adds mock_drive_unprocessed_files & mock_drive_fetch_file fixtures

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+
+from io import BytesIO
 import os
 
 import pytest
+
+import nightshift
 
 
 @pytest.fixture
@@ -15,3 +19,22 @@ def env_var(monkeypatch, local_test_config):
 def test_data(test_session, test_data_core, stub_resource):
     test_session.add(stub_resource)
     test_session.commit()
+
+
+@pytest.fixture
+def mock_drive_unprocessed_files(monkeypatch):
+    def _files(*args):
+        return ["NYP-bar.pout"]
+
+    monkeypatch.setattr(nightshift.tasks, "isolate_unprocessed_files", _files)
+
+
+@pytest.fixture
+def mock_drive_fetch_file(monkeypatch):
+    def _fetch(*args):
+        with open("tests/nyp-ebook-sample.mrc", "rb") as test_file:
+            data = test_file.read()
+            marc_target = BytesIO(data)
+            return marc_target
+
+    monkeypatch.setattr(nightshift.comms.storage.Drive, "fetch_file", _fetch)
