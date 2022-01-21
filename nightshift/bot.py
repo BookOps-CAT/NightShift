ab51d37bdafe1be80b9d48f278ce0c5388bdbd87
Tomek <klingaroo@gmail.com>
2022-01-21 14:27:56 -0500

    adds logging to run()

--- nightshift/bot.py
+++ nightshift/bot.py
@@ -8,7 +8,7 @@ import loggly.handlers
 
 from nightshift.config.logging_conf import log_conf
 
-# from nightshift.manager import process_resources
+from nightshift.manager import process_resources, perform_db_maintenance
 
 conf = log_conf()
 logging.config.dictConfig(conf)
@@ -16,11 +16,14 @@ logger = logging.getLogger("nightshift")
 
 
 def run():
+
     logger.info("Initiating NightShift...")
 
-    # process_resources()
+    process_resources()
+    logger.info("Processing resources complete.")
 
-    logger.info("Processing resources completed.")
+    perform_db_maintenance()
+    logger.info("Performing database maintenence complete.")
 
 
 if __name__ == "__main__":
