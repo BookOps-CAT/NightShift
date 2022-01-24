19d20df6cdb5b0566e3c4c9fdd0ebfb164d5f263
Tomek <klingaroo@gmail.com>
2022-01-24 14:19:23 -0500

    adds local logger test on run()

---
+++ tests/integration/test_bot.py
@@ -0,0 +1,20 @@
+"""
+Top level tests.
+"""
+
+import logging
+
+import pytest
+
+
+from nightshift.bot import run
+
+
+@pytest.mark.local
+def test_logger_on_run(env_var, caplog, test_data):
+
+    with caplog.at_level(logging.INFO):
+        run()
+    assert "Initiating NightShift..." in caplog.text
+    assert "Processing resources completed." in caplog.text
+    assert "Performing database maintenance completed." in caplog.text
