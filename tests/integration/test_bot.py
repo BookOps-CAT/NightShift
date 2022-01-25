b11c3f1c8abb9ffd0a74e0bb2cb9453ef21e21f1
klinga <klingaroo@gmail.com>
2022-01-25 00:00:34 -0500

    fixes initial logging in run process

--- tests/integration/test_bot.py
+++ tests/integration/test_bot.py
@@ -14,7 +14,7 @@ from nightshift.bot import run
 def test_logger_on_run(env_var, caplog, test_data):
 
     with caplog.at_level(logging.INFO):
-        run()
-    assert "Initiating NightShift..." in caplog.text
+        run(env="local")
+    assert "Launching local NightShift..." in caplog.text
     assert "Processing resources completed." in caplog.text
     assert "Performing database maintenance completed." in caplog.text
