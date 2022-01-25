9eefc2b889b8748c8ca5fee20afea72a8b5446cd
Tomek <klingaroo@gmail.com>
2022-01-25 08:57:00 -0500

    adds clean up after test run

--- tests/integration/test_bot.py
+++ tests/integration/test_bot.py
@@ -8,13 +8,22 @@ import pytest
 
 
 from nightshift.bot import run
+from nightshift.comms.storage import Drive, get_credentials
 
 
 @pytest.mark.local
 def test_logger_on_run(env_var, caplog, test_data):
 
     with caplog.at_level(logging.INFO):
-        run(env="local")
-    assert "Launching local NightShift..." in caplog.text
+        run(env="prod")
+
+    assert "Launching prod NightShift..." in caplog.text
     assert "Processing resources completed." in caplog.text
-    assert "Performing database maintenance completed." in caplog.text
+    assert "Database maintenance completed." in caplog.text
+
+    # clean up
+    drive_creds = get_credentials()
+    with Drive(*drive_creds) as drive:
+        temp_files = drive.sftp.listdir(path=drive.dst_dir)
+        for file_handle in temp_files:
+            drive.sftp.remove(f"{drive.dst_dir}/{file_handle}")
